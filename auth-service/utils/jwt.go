024094aacc2c526609c7c2f2adf8ce26f5ce8f18
itsharshitk <harshitknp0@gmail.com>
2025-05-07 16:23:32 +0530

    Update jwt.go

--- auth-service/utils/jwt.go
+++ auth-service/utils/jwt.go
@@ -1,12 +1,13 @@
 package utils
 
 import (
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 )
 
-var jwtKey = []byte("your-secret-key")
+var jwtKey = []byte(os.Getenv("JWT_SECRET_KEY")
 
 func GenerateJWT(email string) (string, error) {
 	claims := jwt.MapClaims{
